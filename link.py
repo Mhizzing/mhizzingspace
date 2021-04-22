24628dfc98f50618e4e8c316a0be805921057419
wlam-git <5207winlam@gmail.com>
2021-04-22 19:35:52 +1000

    First commit

---
+++ link.py
@@ -0,0 +1,30 @@
+import requests
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+username = os.environ.get("st_username")
+token = os.environ.get("st_token")
+auth_header = {'Authorization': f'Bearer {token}'}
+
+base_url = 'https://api.spacetraders.io'
+
+# Game status
+r = requests.get(base_url + '/game/status')
+print(r, r.text)
+
+# Account / Profile
+r = requests.get(base_url + f'/users/{username}', headers=auth_header)
+print(r, r.text)
+
+# View available loans
+r = requests.get(base_url + '/game/loans', headers=auth_header)
+print(r, r.text)
+
+# View ships to purchase
+payload = {'class': 'MK-I'}
+r = requests.get(base_url + '/game/ships', headers=auth_header, params=payload)
+print(r, r.text)
+
